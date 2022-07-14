ccfea0ed1f68f4e723288e4b3413cafade5c0d26
duongphuocsang <duongphuocsang1308@gmail.com>
2022-07-14 21:56:48 +0700

    AVLtree

--- AVLtree.cpp
+++ AVLtree.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int key;
     Node *left;
     Node *right;
@@ -9,20 +10,109 @@ class Node{
 };
 int height(Node *N)
 {
-    if (N==NULL) return 0;
+    if (N == NULL)
+        return 0;
     return N->height;
 }
-Node* newNode(int key)
+Node *newNode(int key)
 {
-    Node* a = new Node();
-    a->key=key;
-    a->left=NULL;
-    a->right=NULL;
-    a->height=1;
+    Node *a = new Node();
+    a->key = key;
+    a->left = NULL;
+    a->right = NULL;
+    a->height = 1;
     return a;
+}
+
+Node *rightRotate(Node *y)
+{
+    Node *x = y->left;
+    Node *T2 = x->right;
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+    return x;
+}
+
+Node *leftRotate(Node *y)
+{
+    Node *x = y->right;
+    Node *T2 = x->left;
+    x->left = y;
+    y->right = T2;
+
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+    return x;
+}
+
+int getBalance(Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+
+Node *insert(Node *node, int key)
+{
+    if (node == NULL)
+        return (newNode(key));
+    if (key < node->key)
+    {
+        node->left = insert(node->left, key);
+    }
+    else if (key > node->key)
+    {
+        node->right = insert(node->right, key);
+    }
+    else
+        return node;
+    node->height = 1 + max(height(node->left), height(node->right));
+    int balance = getBalance(node);
+    if (balance > 1 && key < node->left->key)
+    {
+        return rightRotate(node);
+    }
+    if (balance < -1 && key > node->right->key)
+    {
+        return leftRotate(node);
+    }
+    if (balance > 1 && key > node->left->key)
+    {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+    if (balance < 1 && key < node->right->key)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+    return node;
+}
+
+void preOrder(Node *root)
+{
+    if (root != NULL)
+    {
+        cout << root->key << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
 }
 
 int main()
 {
+    Node *root = NULL;
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
+
+    cout << "Ket qua: ";
+    preOrder(root);
     return 0;
 }
